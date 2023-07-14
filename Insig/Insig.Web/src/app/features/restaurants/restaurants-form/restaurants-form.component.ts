78b6f88b3d1cbcd36d46764f5938abc8b2abf511
Marcin Mitręga <m.mitrega27@gmail.com>
2023-07-14 20:18:08 +0200

    Create adding restaurant form with correct values

--- Insig/Insig.Web/src/app/features/restaurants/restaurants-form/restaurants-form.component.ts
+++ Insig/Insig.Web/src/app/features/restaurants/restaurants-form/restaurants-form.component.ts
@@ -1,23 +1,33 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-restaurants-form',
   templateUrl: './restaurants-form.component.html',
   styleUrls: ['./restaurants-form.component.scss']
 })
-
 export class RestaurantsFormComponent {
-  firstName: string = '';
-  lastName: string = '';
+  restaurantName: string = '';
+  location: string = '';
+  stars!: number;
+  averagePrice!: number;
+  kitchenType: string = 'italian';
+  delivery: boolean = false;
 
   submitForm() {
     // Handle form submission here
-    console.log('First Name:', this.firstName);
-    console.log('Last Name:', this.lastName);
+    console.log('Restaurant Name:', this.restaurantName);
+    console.log('Location:', this.location);
+    console.log('Restaurant Stars:', this.stars);
+    console.log('Average Price of Food:', this.averagePrice);
+    console.log('Kitchen Type:', this.kitchenType);
+    console.log('Delivery Option:', this.delivery);
 
     // Reset form fields
-    this.firstName = '';
-    this.lastName = '';
+    this.restaurantName = '';
+    this.location = '';
+    this.stars = null!;
+    this.averagePrice = null!;
+    this.kitchenType = 'italian';
+    this.delivery = false;
   }
 }
