bfe238f8be8f4b3870ae4fbbff2defeb9b3a2aeb
Marcin Mitręga <m.mitrega27@gmail.com>
2023-07-14 22:01:52 +0200

    Change colors palette

--- Insig/Insig.Web/src/app/features/restaurants/restaurants-list/restaurants-list.component.ts
+++ Insig/Insig.Web/src/app/features/restaurants/restaurants-list/restaurants-list.component.ts
@@ -18,20 +18,20 @@ export class RestaurantsListComponent implements OnInit {
   totalItems: number = 0;
 
   ngOnInit() {
-    // Initialize the list with some sample data
+  
     this.restaurants = [
       { name: 'Restaurant 1', location: 'Location 1', stars: 4, averagePrice: 25.5, kitchenType: 'Italian', delivery: true },
       { name: 'Restaurant 2', location: 'Location 2', stars: 3, averagePrice: 15.0, kitchenType: 'Indian', delivery: false },
       { name: 'Restaurant 3', location: 'Location 3', stars: 5, averagePrice: 35.0, kitchenType: 'Mexican', delivery: true }
     ];
 
-    // Calculate total items count
+
     this.totalItems = this.restaurants.length;
   }
 
   addRestaurant(restaurant: Restaurant) {
     this.restaurants.push(restaurant);
-    this.totalItems++; // Increment total items count
+    this.totalItems++; 
   }
 
   onPageChange(page: number) {
