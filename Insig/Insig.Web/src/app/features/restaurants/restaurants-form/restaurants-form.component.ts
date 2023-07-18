b32ff29eb7f82b36cc78b53b7d02a970c48b48ba
Marcin Mitręga <m.mitrega27@gmail.com>
2023-07-18 21:54:28 +0200

    I am debil, goofy bug fix :DDD

--- Insig/Insig.Web/src/app/features/restaurants/restaurants-form/restaurants-form.component.ts
+++ Insig/Insig.Web/src/app/features/restaurants/restaurants-form/restaurants-form.component.ts
@@ -72,33 +72,25 @@ export class RestaurantsFormComponent  implements OnInit  {
   }
  
 
-  ngOnInit(): void {
-    this.restaurants = this._restaurantService.getRestaurantData();
-    
+ngOnInit(): void {
+  this.restaurants = this._restaurantService.getRestaurantData();
 
-    if(this._name!=null){
-      this.activatedRoute.paramMap.subscribe((param: Params) => {
-        this._name = param['get']('id') || '';
-        console.log("aaa:", this._name);
+  this.activatedRoute.paramMap.subscribe((param: Params) => {
+    this._name = param['get']('id') || '';
+    console.log("aaa:", this._name);
 
-        this._restaurantService.editRestaurantData().subscribe((data: RestaurantDto) => {
-          this.restaurant = data;
-          console.log("bbb:", this.restaurant);
-
-        });
-      });
-
+    if (this._name !== '') {
       this.restaurants.subscribe((res: RestaurantDto[]) => {
         const foundRestaurant = res.find(r => r.name === this._name);
         if (foundRestaurant) {
           this.restaurant = foundRestaurant;
           console.log("xd:", this.restaurant.name);
-        } 
-          
+        }
       });
     }
-    
-  }
+  });
+}
+
   
   
 
